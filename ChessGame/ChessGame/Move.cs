3aaa16d2fb8c6ae1c4f52824bdca3b03b7fef27c
Jack Taylor <jtaylor1205@hotmail.co.uk>
2015-09-20 13:18:14 +0100

    Moving some stuff out to methods for calculating closest pieces

--- ChessGame/ChessGame/Move.cs
+++ ChessGame/ChessGame/Move.cs
@@ -14,11 +14,13 @@ namespace ChessGame
                 case PieceType.Pawn:
                     if (piece.Colour == PieceColour.White)
                     {
+                        
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, 1));
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, 2));
                     }
                     else if (piece.Colour == PieceColour.Black)
                     {
+                        var lel = GetMaxMovesUp(pieces, piece);
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, -1));
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, -2));
                     }
@@ -60,45 +62,28 @@ namespace ChessGame
                     break;
                 case PieceType.Rook:
                 case PieceType.Queen:
-                    var closestPieceUp =
-                   pieces.Where(p => p.TilePosition.X < piece.TilePosition.X && p.TilePosition.Y == piece.TilePosition.Y)
-                       .OrderByDescending(p => p.TilePosition.X)
-                       .FirstOrDefault();
+                    var maxMovesUp = GetMaxMovesUp(pieces, piece);
 
-                    var maxMovesUp = closestPieceUp != null ? (piece.TilePosition.X - closestPieceUp.TilePosition.X) : piece.TilePosition.X + 1;
                     for (int i = 1; i < maxMovesUp; i++)
                     {
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, -i));
                     }
 
-                    var closestPieceDown =
-                        pieces.Where(p => p.TilePosition.X > piece.TilePosition.X && p.TilePosition.Y == piece.TilePosition.Y)
-                            .OrderBy(p => p.TilePosition.X)
-                            .FirstOrDefault();
+                    var maxMovesDown = GetMaxMovesDown(pieces, piece);
 
-                    var maxMovesDown = closestPieceDown != null ? (Math.Abs(piece.TilePosition.X - closestPieceDown.TilePosition.X)) : 8 - piece.TilePosition.X;
                     for (int i = 1; i < maxMovesDown; i++)
                     {
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, 0, piece.TilePosition.X, i));
                     }
 
-                    var closestPieceLeft =
-                        pieces.Where(p => p.TilePosition.Y < piece.TilePosition.Y && p.TilePosition.X == piece.TilePosition.X)
-                            .OrderByDescending(p => p.TilePosition.Y)
-                            .FirstOrDefault();
+                    var maxMovesLeft = GetMaxMovesLeft(pieces, piece);
 
-                    var maxMovesLeft = closestPieceLeft != null ? (piece.TilePosition.Y - closestPieceLeft.TilePosition.Y) : piece.TilePosition.Y + 1;
                     for (int i = 1; i < maxMovesLeft; i++)
                     {
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, -i, piece.TilePosition.X, 0));
                     }
 
-                    var closestPieceRight =
-                        pieces.Where(p => p.TilePosition.Y > piece.TilePosition.Y && p.TilePosition.X == piece.TilePosition.X)
-                            .OrderBy(p => p.TilePosition.Y)
-                            .FirstOrDefault();
-
-                    var maxMovesRight = closestPieceRight != null ? (Math.Abs(piece.TilePosition.Y - closestPieceRight.TilePosition.Y)) : 8 - piece.TilePosition.Y;
+                    var maxMovesRight = GetMaxMovesRight(pieces, piece);
                     for (int i = 1; i < maxMovesRight; i++)
                     {
                         moves.Add(Helpers.MoveXY(piece.TilePosition.Y, i, piece.TilePosition.X, 0));
@@ -108,6 +93,58 @@ namespace ChessGame
             }
 
             return moves;
+        }
+        private static int GetMaxMovesUp(List<Piece> pieces, Piece piece)
+        {
+                   var closestPieceUp =
+                   pieces.Where(p => p.TilePosition.X < piece.TilePosition.X && p.TilePosition.Y == piece.TilePosition.Y)
+                       .OrderByDescending(p => p.TilePosition.X)
+                       .FirstOrDefault();
+
+                    var maxMovesUp = closestPieceUp != null ? (piece.TilePosition.X - closestPieceUp.TilePosition.X) : piece.TilePosition.X + 1;
+
+            return maxMovesUp;
+        }
+        private static int GetMaxMovesDown(List<Piece> pieces, Piece piece)
+        {
+            var closestPieceDown =
+                       pieces.Where(p => p.TilePosition.X > piece.TilePosition.X && p.TilePosition.Y == piece.TilePosition.Y)
+                           .OrderBy(p => p.TilePosition.X)
+                           .FirstOrDefault();
+
+            var maxMovesDown = closestPieceDown != null ? (Math.Abs(piece.TilePosition.X - closestPieceDown.TilePosition.X)) : 8 - piece.TilePosition.X;
+            return maxMovesDown;
+        }
+        private static int GetMaxMovesLeft(List<Piece> pieces, Piece piece)
+        {
+            var closestPieceLeft =
+                                   pieces.Where(p => p.TilePosition.Y < piece.TilePosition.Y && p.TilePosition.X == piece.TilePosition.X)
+                                       .OrderByDescending(p => p.TilePosition.Y)
+                                       .FirstOrDefault();
+
+            var maxMovesLeft = closestPieceLeft != null ? (piece.TilePosition.Y - closestPieceLeft.TilePosition.Y) : piece.TilePosition.Y + 1;
+            return maxMovesLeft;
+        }
+        private static int GetMaxMovesRight(List<Piece> pieces, Piece piece)
+        {
+            var closestPieceRight =
+                       pieces.Where(p => p.TilePosition.Y > piece.TilePosition.Y && p.TilePosition.X == piece.TilePosition.X)
+                           .OrderBy(p => p.TilePosition.Y)
+                           .FirstOrDefault();
+
+            var maxMovesRight = closestPieceRight != null ? (Math.Abs(piece.TilePosition.Y - closestPieceRight.TilePosition.Y)) : 8 - piece.TilePosition.Y;
+            return maxMovesRight;
+        }
+        /// <summary>
+        /// thought this would be nice but can't be used
+        /// </summary>
+        /// <param name="axis"></param>
+        /// <param name="pieces"></param>
+        /// <param name="piece"></param>
+        /// <returns></returns>
+        private static Array[] GetMaxMovesForAxis(char axis, List<Piece> pieces, Piece piece)
+        {
+            return null;
         }
     }
 }
